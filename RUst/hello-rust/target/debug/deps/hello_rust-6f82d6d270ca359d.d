0cf14c8ad767e29b9d3dca8c65933a86eed7ee24
Bijay Shrestha <110684170+Isojay@users.noreply.github.com>
2024-05-03 06:39:32 +0000

    stack uop

---
+++ RUst/hello-rust/target/debug/deps/hello_rust-6f82d6d270ca359d.d
@@ -0,0 +1,5 @@
+/workspaces/Stack_up_Quest/RUst/hello-rust/target/debug/deps/hello_rust-6f82d6d270ca359d: src/main.rs
+
+/workspaces/Stack_up_Quest/RUst/hello-rust/target/debug/deps/hello_rust-6f82d6d270ca359d.d: src/main.rs
+
+src/main.rs:
---
+++ RUst/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/workspaces/Stack_up_Quest/RUst/hello-rust/target/debug/hello-rust: /workspaces/Stack_up_Quest/RUst/hello-rust/src/main.rs
